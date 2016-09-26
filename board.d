1d73b5c8d49f0228fd5be81016300a2d6ac23457
Mo Hyder <smhyder@uwaterloo.ca>
2016-09-26 12:01:03 -0400

    old project

---
+++ board.d
@@ -0,0 +1,2 @@
+board.o: board.cc board.h piece.h king.h queen.h bishop.h knight.h pawn.h \
+ rook.h
---
+++ main.d
@@ -0,0 +1,2 @@
+main.o: main.cc board.h piece.h player.h human.h comp1.h comp2.h comp3.h \
+ view.h text.h graphic.h window.h
